695ccfebc5d3971a1df56b6f83f83e06d59c0724
Peter Sorensen <peter.sorensen@fueled.com>
2025-12-24 10:20:52 -0800

    update assets

--- build/liveblog-block/index.asset.php
+++ build/liveblog-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '90a173cb3365a3f84dca');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '51897960384acffef613');
--- build/liveblog-block/view.asset.php
+++ build/liveblog-block/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'c4c3e5d7489ad4656026');
+<?php return array('dependencies' => array(), 'version' => '97759a01d8f40efcb586');
